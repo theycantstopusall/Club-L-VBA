cf1b5d4718f519ddce5860c86f10ce98800beeef
theycantstopusall <64639380+theycantstopusall@users.noreply.github.com>
2020-10-05 14:37:16 +0100

    Update Copy.vb
    Updated with input from Harriet

--- Copy.vb
+++ Copy.vb
@@ -1,7 +1,15 @@
+Sub copy()
+Dim Wb As Workbook
+Dim Wb2 As Workbook
+Set Wb = ThisWorkbook
 
-Sub 
-
-Workbooks("PVX Data.xlsx").Worksheets("PVX DATA").Range("A2:BE").Copy
-    Workbooks("Checking file.xlsm").Worksheets("Despatch summary").Range("A2")
+    ' Range("A2:B2" & Range("A" & Rows.Count).End(xlUp).Row).FillDown
+        Workbooks.Open ("\\SERVER2019\Data\Finance\Carriage\for macro\Checking file.xlsx")
+        Set Wb2 = ThisWorkbook
+        Wb.Activate
+        Wb.Worksheets("PVX Data").Range("A2:BB10000").copy
+            Wb2.Activate
+            
+            Wb2.Worksheets("Despatch summary").Range("A2:BB10000").PasteSpecial Paste:=xlPasteValues
 
 End Sub
